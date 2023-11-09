bd5ebca86fd4c4317bc7aea1948420d82e466de0
neerajverma111 <neerajryan54321@gmail.com>
2023-11-09 14:02:32 +0530

    Neeraj

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+import './../index.css';
+
+import logo from '../../images/logo.png';
+
+
+const NavbarItem = ({title, classProps})=>{
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    );
+    
+}
+const Navbar = () =>{
+    return (
+        <nav className = "w-full flex md:justify-center justify-between item-center p-4">
+        <div className="md:flex-[0.5] flex-initial justify-center item-center">
+        <img src={logo} alt="logo" className="w-32" style={{ cursor: 'pointer',margin: "15px" }} />
+        </div>
+        
+        <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+        {["Market" , "Exchange", "Tutorials" , "Wallets"].map((item,index) =>(
+              <NavbarItem key = { item +index} title= {item}/>
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+            Login
+        </li>
+        </ul>
+        </nav>
+    );
+}
+export default Navbar;
+
